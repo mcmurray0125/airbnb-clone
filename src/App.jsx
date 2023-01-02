f5a5c6f10673114f0545e9b7f4ebcc3df445baac
Michael Murray <mcmurray0125@gmail.com>
2023-01-01 21:27:36 -0600

    finished airbnb clone card project

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,21 @@ import Hero from './components/Hero'
 import Card from './components/Card'
 import data from './data'
 
-/*
-Challenge:
-
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
 
 export default function App() {
-          //Hero//
-
+  
   const cardElements = data.map(i => {
     return (
       <Card
           key={i.id}
-          rating={i.stats.rating}
-          reviewCount={i.stats.reviewCount}
-          img={i.coverImg}
-          title={i.title}
-          description={i.description}
-          price={i.price}
-          stats={i.stats}
-          location={i.location}
-          openSpots={i.openSpots}
+          item={i}
           />
     )
   })
   return (
     <div>
         <Navbar/>
+        <Hero/>
         <section className='cards-list'>
         {cardElements}
         </section>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,33 +4,26 @@ import star from "/src/images/star.png"
 
 export default function Card(props) {
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    }  else if (props.location === "Online") {
+    }  else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     } 
 
 
-    /*
-    Challenge:
-    1. Display the correct text in the badge based on the logic above
-    2. Only display the badge if badgeText has a value
-    */
-
-
     return (
         <div className="card">
-            {props.openSpots === 0 && <div className="card--status">
+            {badgeText && <div className="card--status">
                 {badgeText}
             </div>}
-            <img src={`/src/images/${props.img}`} alt="katie-zeferes" />
+            <img src={`/src/images/${props.item.coverImg}`} alt="katie-zeferes" />
                 <div className="card--stats">
                     <img src={star}  alt="star" className="star"/>
-                    <small className="card--rating">&nbsp;{props.rating}&nbsp;</small>
-                    <small className="card--more--info">({props.reviewCount}) &bull; {props.location}</small>
+                    <small className="card--rating">&nbsp;{props.item.stats.rating}&nbsp;</small>
+                    <small className="card--more--info">({props.item.stats.reviewCount}) &bull; {props.item.location}</small>
                 </div>
-                <small className="card--desc">{props.title}</small>
-                <small className="card--price"><strong>From ${props.price} </strong>/ person</small>
+                <small className="card--desc">{props.item.title}</small>
+                <small className="card--price"><strong>From ${props.item.price} </strong>/ person</small>
             </div>
     )
 }
